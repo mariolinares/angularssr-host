b9bb3790e086f3889a077905c288b5617addc0d9
Mario Linares <mario.linares@vass.es>
2025-03-23 11:06:50 +0100

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
+import { loadRemoteModule } from '@angular-architects/native-federation';
 import { Routes } from '@angular/router';
+import { DummyComponent } from './dummy/dummy.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () =>
+      loadRemoteModule({
+        remoteName: 'angularremote',
+        exposedModule: './Component',
+        fallback: DummyComponent,
+      }).then((m) => m.AppComponent),
+  },
+  {
+    path: 'form',
+    loadComponent: () =>
+      import('./pages/address-form/address-form.component').then(
+        (m) => m.AddressFormComponent
+      ),
+  },
+];
